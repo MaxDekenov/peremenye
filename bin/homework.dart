5d9b2dc48aeb6faae36fbc15972923abc660b7c6
MaxDekenov <de.maksat@gmail.com>
2023-03-17 16:49:19 +0600

    first commit

---
+++ bin/homework.dart
@@ -0,0 +1,17 @@
+
+void main() {
+  double b = 4.20;
+  int a =3;
+  print(a+b);
+
+  String text = 'Name';
+  text ='Flutter';
+  print('I want to learn $text');
+
+ double myNumber = 2.225;
+ String myText;
+ myText='$myNumber';
+ print(myText);
+
+
+}
